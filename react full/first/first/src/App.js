4cbfbe354ab8605773ada02da5135eecc636bc8f
jarek <js100code@gmail.com>
2022-11-16 21:22:28 +0100

    Add second component. Use props

--- react full/first/first/src/App.js
+++ react full/first/first/src/App.js
@@ -1,12 +1,26 @@
-import logo from './logo.svg';
 import './App.css';
+
+const Person = (props) => {
+  return (
+    <>
+      <h1>Name: {props.name}</h1>
+      <h2>Last Name: {props.name} </h2>
+
+      <h2>Age: {props.age}</h2>
+
+    </>
+  )
+}
+
 
 const App= ()=> {
   const name = 'John' ;
   const isNameShowing = false ;
   return (
     <div className="App">
-      <h1> Hello React {isNameShowing ? name : 'someone else'}</h1>
+     <Person name={"John"} />
+     <Person name={'Janek'} lastname={'kowalski'} age={39}/>
+     <Person />
     </div>
   );
 }
