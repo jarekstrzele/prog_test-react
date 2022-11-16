196b17415a5f6d39409f040b16ce8c0a27b6dc20
jarek <js100code@gmail.com>
2022-11-16 20:58:04 +0100

    first

---
+++ react full/first/first/src/App.js
@@ -0,0 +1,14 @@
+import logo from './logo.svg';
+import './App.css';
+
+const App= ()=> {
+  const name = 'John' ;
+  const isNameShowing = false ;
+  return (
+    <div className="App">
+      <h1> Hello React {isNameShowing ? name : 'someone else'}</h1>
+    </div>
+  );
+}
+
+export default App;
