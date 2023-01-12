cafa471fccedead4fc67fff0f9865ff8bcd7f655
jarek <js100code@gmail.com>
2023-01-12 22:15:32 +0100

    Add name & age handlers

--- login_app/src/components/Users/AddUser.js
+++ login_app/src/components/Users/AddUser.js
@@ -1,12 +1,25 @@
-import React from 'react' ;
+import React, { useState } from 'react' ;
 import Card from '../UI/Card' ;
 import classes from './AddUser.module.css' ;
-import Cutton from '../UI/Button';
+import Button from '../UI/Button';
 
 const AddUser = props => {
+    const [enteredName, setEnteredName] = useState('');
+    const [enteredAge, setEnteredAge] = useState('');
 
     const addUserHandler = (event) => {
         event.preventDefault();
+        console.log(enteredName, enteredAge) ;
+        setEnteredName('');
+        setEnteredAge('')
+    }
+
+    const nameChangeHandkler = (event) =>{
+        setEnteredName(event.target.value) ;
+    }
+
+    const ageChangeHandkler = (event) =>{
+        setEnteredAge(event.target.value) ;
     }
 
     return (
@@ -14,9 +27,13 @@ const AddUser = props => {
         <form onSubmit={addUserHandler}>
             
             <lable class="">Username</lable>
-            <input id="username" type="text" />
+            <input id="username" type="text" 
+                onChange={nameChangeHandkler}
+                value={enteredName}/>
             <lable class="">Age</lable>
-            <input id="age" type="number" />
+            <input id="age" type="number"
+                    onChange={ageChangeHandkler}
+                    value={enteredAge} />
             
             <button type="submit">Add user</button>
         </form>
